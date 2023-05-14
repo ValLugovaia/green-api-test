b83cf19e4bcd0191439364e64d54757a35d53b45
Valentina Lugovaia <vaal.it@yandex.ru>
2023-05-14 22:38:51 +0300

    delete signup

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,12 @@ import { Navigate , Route, Routes, useNavigate } from 'react-router-dom';
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 import mainApi from '../api/MainApi.js';
 import Main from './Main.js';
-import Register from './Register.js';
 import Login from './Login.js';
 import ProtectedRoute from './ProtectedRoute.js';
 
 function App() {
   const history = useNavigate();
   const [loggedIn, setLoggedIn] = useState(false);
-  const [isSignup, setIsSignup] = useState(false);
-
-  function onRegister(id, token) {
-    return mainApi.signup(id, token)
-      .then((data) => {
-        setIsSignup(true);
-        history.push("/signin");
-      })
-      .catch(err => {
-        console.log(err);
-        setIsSignup(false);
-      });
-  }
 
   function onLogin(id, token) {
     return mainApi.signin(id, token)
@@ -39,17 +25,13 @@ function App() {
       });
   }
 
-
   return (
     <CurrentUserContext.Provider>
       <div className="root">
         <Routes>
-          <ProtectedRoute exact path="/" loggedIn={loggedIn} component={Main} />
-          <Route path="/signup">
-            <Register onRegister={onRegister} isSignup={isSignup} />
-          </Route>
+          <ProtectedRoute path="/" loggedIn={loggedIn} component={Main} />
           <Route path="/signin">
-            <Login onLogin={onLogin} isSignup={isSignup} />
+            <Login onLogin={onLogin} />
           </Route>
           <Route>{loggedIn ? <Navigate to="/" /> : <Navigate to="/signin" />}</Route>
         </Routes>
